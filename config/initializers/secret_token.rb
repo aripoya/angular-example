64e9773517d6d57fe64e700bc59ae7eb8661e7ea
Michael Makarov <info1117@gmail.com>
2013-05-14 17:07:34 -0700

    Angular Rails4 App: no turbolinks: yeey!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rsapp::Application.config.secret_key_base = 'f64c58d25fb9c7b25f5b325c172e9e14b54c49ba24977b6b68cc999392ab1d8f451f30b93888c589a2cb21dd75a341a3f7961f1a8502eed296ca6206ab8e8f19'
